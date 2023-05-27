49e49503fc6f3c960beaa11fd98006a817d1d484
Jim-Clean <128581808+Jim-Clean@users.noreply.github.com>
2023-05-27 10:55:59 +0100

    Add program breakdown into smaller problems

---
+++ myscripts.js
@@ -0,0 +1,14 @@
+/*
+Make game of rock paper scissors using js. Need user to select entry and
+computer to generate random entry of the three options.
+
+- Get computer choice. randomly return one value of either "Rock", "Paper" or
+"Scissors".
+
+- Allow user to enter choice manually. Make this case insensitive. As long as
+characters are in correct order it will accept selection.
+
+- Allow user to play against computer and display the winner.
+
+- Expand game to 5 rounds and keep score until there is a winner.
+*/
